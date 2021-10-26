3277caf90a11b589c4b36a10632065412e33ced0
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-10-26 03:34:44 -0400

    async via arrow function

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,24 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = { images: [] };
+  // whenever we expect a state property to eventually contain an Array that's what we default it to.
+
+  onSearchSubmit = async (term) => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization: "Client-ID h0oHMJ3efbKgGYBTJ7KIULU9EOR4mrjbGRTC7uhlqng",
       },
     });
-    console.log(response.data.results);
-  }
+    this.setState({ images: response.data.results }); //This will trigger rerender
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found
       </div>
     );
   }
